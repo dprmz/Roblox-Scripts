40ad7bf6a68a37e184a38b1704b384d23742bd55
dprmz <150216745+dprmz@users.noreply.github.com>
2026-08-06 07:52:40 +0700

    Update Config.lua

--- Modules/Config.lua
+++ Modules/Config.lua
@@ -1,1 +1,19 @@
-/
+-- Config.lua
+return {
+    -- Survivor
+    SpeedEnabled = false,
+    SpeedValue = 16,
+    AutoAim = false,
+    AimPart = "Head",   -- "Head" or "Torso"
+    AutoParry = false,
+    ParryRadius = 15,
+    FastVault = false,
+
+    -- ESP
+    WallhackSurvivor = false,
+    WallhackKiller = false,
+    HookESP = false,
+
+    -- Visual (future)
+    -- ...
+}
